fea19fbf84914392aae0f7798ff64e11f71c9652
ColinH1127 <colinhussey82@gmail.com>
2025-02-01 17:19:28 -0500

    added text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -13,7 +13,13 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is not a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
-    
+
+class TestTextNodeToHTML:
+    def test_eq(self):
+        node = TextNode("This is not a text node", TextType.BOLD)
+        expected_result = "<b>This is not a text node</b>"
+        self.assertEqual(node.text_node_to_html_node,)
+
 
 
 if __name__ == "__main__":
