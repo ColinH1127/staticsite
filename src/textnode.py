079419c974d977a2a3a082f216f111fbf0a820e2
ColinH1127 <colinhussey82@gmail.com>
2025-02-02 22:17:26 -0500

    created split_nodes functions

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from htmlnode import *
+import re
 
 class TextType(Enum):
     NORMAL = "normal"
@@ -56,4 +57,27 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         else: 
             raise Exception("please use a proper delimiter")
         return new_nodes
-        
+def extract_markdown_images(text):
+    images = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return images
+def extract_markdown_links(text):
+    links = re.findallr("(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)   
+    return links
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        parts = node.split("[", 1)
+        text = parts[0]
+        image = extract_markdown_images(node)
+        new_nodes.append(TextNode(text, TextType.NORMAL))
+        new_nodes.append(TextNode(image, TextType.IMAGE))
+    return new_nodes
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        parts = node.split("[", 1)
+        text = parts[0]
+        link = extract_markdown_links(node)
+        new_nodes.append(TextNode(text, TextType.NORMAL))
+        new_nodes.append(TextNode(link, TextType.IMAGE))
+    return new_nodes
